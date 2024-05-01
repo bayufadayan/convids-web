de647c750b7f637956d6b501dc357db1cffc8c5f
bayufadayan <bayufadayan@gmail.com>
2024-05-01 23:59:16 +0700

    done for now

--- script/script.js
+++ script/script.js
@@ -5,6 +5,8 @@ hamburger.addEventListener('click', () => {
     nav_list.classList.toggle('aktif');
 });
 
+// -------
+
 const nav_menu_items = document.querySelectorAll('.nav-list li');
 nav_menu_items.forEach(item => {
     item.addEventListener('click', () => {
@@ -18,3 +20,31 @@ nav_menu_items.forEach(link => {
         window.location.href = href;
     })
 })
+
+// ---------
+document.querySelectorAll('.for-popup').forEach(image => {
+    image.onclick = () => {
+        document.querySelector('.popup-img').style.display = 'block';
+        document.querySelector('.popup-img img').src = image.getAttribute('src');
+    }
+})
+
+document.querySelector('.popup-img span').onclick = () =>{
+    document.querySelector('.popup-img').style.display = 'none';
+}
+
+// ----------
+document.getElementById('trailer-play').addEventListener('click', function() {
+    window.open('https://youtu.be/DKjyrQxibo8?si=raJaVd59pWeL07Jw', '_blank');
+});
+
+document.getElementById('film-play').addEventListener('click', function() {
+    window.open('https://youtu.be/AApRjBZRzbI?si=OSFc_F88xR2kN9h4', '_blank');
+});
+
+const coming_soon = document.querySelectorAll('.coming-soon');
+coming_soon.forEach(button => {
+    button.addEventListener('click', function() {
+        window.location.href = 'coming-soon.html';
+    });
+});
